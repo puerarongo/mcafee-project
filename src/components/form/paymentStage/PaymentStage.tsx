08d8e81379beb9aa7e3d859fdc908bddfe14a9f4
puerarongo <akhardin96@gmail.com>
2023-08-04 15:04:33 +0300

    add paypal variation

--- src/components/form/paymentStage/PaymentStage.tsx
+++ src/components/form/paymentStage/PaymentStage.tsx
@@ -16,6 +16,8 @@ const PaymentStage: React.FC<IPaymentStage> = ({ setActiveForm, email }) => {
     setActiveForm(false);
   };
 
+  console.log(isActive);
+
   return (
     <div className={styles.container}>
       <div className={styles.title__container}>
@@ -52,244 +54,252 @@ const PaymentStage: React.FC<IPaymentStage> = ({ setActiveForm, email }) => {
           PayPal
         </button>
       </div>
-      <Formik
-        initialValues={{
-          cardNumber: "",
-          firstName: "",
-          lastName: "",
-          date: "",
-          securityCode: "",
-          zip: "",
-        }}
-        validationSchema={paymentValidationSchema}
-        onSubmit={(values) => {
-          console.log("Form values", values);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form className={styles.email__form} onSubmit={handleSubmit}>
-            <TextField
-              sx={
-                errors.cardNumber && touched.cardNumber
-                  ? {
-                      ...textFieldCostumization,
-                      width: "555px",
-                      border: "1px solid #e32224",
-                    }
-                  : {
-                      ...textFieldCostumization,
-                      width: "555px",
-                      border: "1px solid #bdbdbd",
-                    }
-              }
-              label="Card Number"
-              name="cardNumber"
-              type="number"
-              variant="filled"
-              value={values.cardNumber}
-              onBlur={handleBlur}
-              onChange={handleChange}
-            />
-            {errors.cardNumber && touched.cardNumber ? (
-              <p className={styles.validation__error}>{errors.cardNumber}</p>
-            ) : (
-              <span className={styles.default}></span>
-            )}
-
-            <div className={styles.name__container}>
-              <div className={styles.field__container}>
-                <TextField
-                  sx={
-                    errors.firstName && touched.firstName
-                      ? {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #e32224",
-                        }
-                      : {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #bdbdbd",
-                        }
-                  }
-                  label="First Name"
-                  type="text"
-                  name="firstName"
-                  variant="filled"
-                  value={values.firstName}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                {errors.firstName && touched.firstName ? (
-                  <p className={styles.validation__error}>{errors.firstName}</p>
-                ) : (
-                  <span className={styles.default}></span>
-                )}
-              </div>
-
-              <div className={styles.field__container}>
-                <TextField
-                  sx={
-                    errors.lastName && touched.lastName
-                      ? {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #e32224",
-                        }
-                      : {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #bdbdbd",
-                        }
-                  }
-                  label="Last Name"
-                  type="text"
-                  name="lastName"
-                  variant="filled"
-                  value={values.lastName}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                {errors.lastName && touched.lastName ? (
-                  <p className={styles.validation__error}>{errors.lastName}</p>
-                ) : (
-                  <span className={styles.default}></span>
-                )}
-              </div>
-            </div>
-
-            <div className={styles.code__container}>
-              <div className={styles.field__container}>
-                <TextField
-                  sx={
-                    errors.date && touched.date
-                      ? {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #e32224",
-                        }
-                      : {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #bdbdbd",
-                        }
-                  }
-                  label="Expiry Date (MM/YY)"
-                  type="date"
-                  name="date"
-                  variant="filled"
-                  value={values.date}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                {errors.date && touched.date ? (
-                  <p className={styles.validation__error}>{errors.date}</p>
-                ) : (
-                  <span className={styles.default}></span>
-                )}
-              </div>
-
-              <div className={styles.field__container}>
-                <TextField
-                  sx={
-                    errors.securityCode && touched.securityCode
-                      ? {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #e32224",
-                        }
-                      : {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #bdbdbd",
-                        }
-                  }
-                  label="Security Code (CVV)"
-                  type="password"
-                  name="securityCode"
-                  variant="filled"
-                  value={values.securityCode}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                {errors.securityCode && touched.securityCode ? (
-                  <p className={styles.validation__error}>
-                    {errors.securityCode}
-                  </p>
-                ) : (
-                  <span className={styles.default}></span>
-                )}
-              </div>
-            </div>
-
-            <div className={styles.name__container}>
-              <div className={styles.field__container}>
-                <TextField
-                  sx={
-                    errors.zip && touched.zip
-                      ? {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #e32224",
-                        }
-                      : {
-                          ...textFieldCostumization,
-                          width: "242px",
-                          border: "1px solid #bdbdbd",
-                        }
-                  }
-                  label="Billing Zip Code"
-                  type="text"
-                  name="zip"
-                  variant="filled"
-                  value={values.zip}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                {errors.zip && touched.zip ? (
-                  <p className={styles.validation__error}>{errors.zip}</p>
-                ) : (
-                  <span className={styles.default}></span>
-                )}
-              </div>
-            </div>
-
-            <div className={styles.checkbox__container}>
-              <input
-                className={styles.checkbox}
-                type="checkbox"
-                name="checkbox"
+      {isActive ? (
+        <Formik
+          initialValues={{
+            cardNumber: "",
+            firstName: "",
+            lastName: "",
+            date: "",
+            securityCode: "",
+            zip: "",
+          }}
+          validationSchema={paymentValidationSchema}
+          onSubmit={(values) => {
+            console.log("Form values", values);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form className={styles.email__form} onSubmit={handleSubmit}>
+              <TextField
+                sx={
+                  errors.cardNumber && touched.cardNumber
+                    ? {
+                        ...textFieldCostumization,
+                        width: "555px",
+                        border: "1px solid #e32224",
+                      }
+                    : {
+                        ...textFieldCostumization,
+                        width: "555px",
+                        border: "1px solid #bdbdbd",
+                      }
+                }
+                label="Card Number"
+                name="cardNumber"
+                type="number"
+                variant="filled"
+                value={values.cardNumber}
+                onBlur={handleBlur}
+                onChange={handleChange}
               />
-              <p className={styles.checkbox__text}>
-                By clicking Place Order, I accept the License Agreement,
-                Auto-Renewal Policy and Privacy Notice.
-              </p>
-            </div>
+              {errors.cardNumber && touched.cardNumber ? (
+                <p className={styles.validation__error}>{errors.cardNumber}</p>
+              ) : (
+                <span className={styles.default}></span>
+              )}
 
-            <div className={styles.container__confirm}>
-              <p className={styles.text__confirm}>
-                This payment will be processed in the United States
-              </p>
-              <button
-                className={styles.button__confirm}
-                type="submit"
-                onClick={() => {
-                  touched.cardNumber = true;
-                }}
-              >
-                Place Order
-              </button>
-            </div>
-          </form>
-        )}
-      </Formik>
+              <div className={styles.name__container}>
+                <div className={styles.field__container}>
+                  <TextField
+                    sx={
+                      errors.firstName && touched.firstName
+                        ? {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #e32224",
+                          }
+                        : {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #bdbdbd",
+                          }
+                    }
+                    label="First Name"
+                    type="text"
+                    name="firstName"
+                    variant="filled"
+                    value={values.firstName}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.firstName && touched.firstName ? (
+                    <p className={styles.validation__error}>
+                      {errors.firstName}
+                    </p>
+                  ) : (
+                    <span className={styles.default}></span>
+                  )}
+                </div>
+
+                <div className={styles.field__container}>
+                  <TextField
+                    sx={
+                      errors.lastName && touched.lastName
+                        ? {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #e32224",
+                          }
+                        : {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #bdbdbd",
+                          }
+                    }
+                    label="Last Name"
+                    type="text"
+                    name="lastName"
+                    variant="filled"
+                    value={values.lastName}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.lastName && touched.lastName ? (
+                    <p className={styles.validation__error}>
+                      {errors.lastName}
+                    </p>
+                  ) : (
+                    <span className={styles.default}></span>
+                  )}
+                </div>
+              </div>
+
+              <div className={styles.code__container}>
+                <div className={styles.field__container}>
+                  <TextField
+                    sx={
+                      errors.date && touched.date
+                        ? {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #e32224",
+                          }
+                        : {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #bdbdbd",
+                          }
+                    }
+                    label="Expiry Date (MM/YY)"
+                    type="date"
+                    name="date"
+                    variant="filled"
+                    value={values.date}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.date && touched.date ? (
+                    <p className={styles.validation__error}>{errors.date}</p>
+                  ) : (
+                    <span className={styles.default}></span>
+                  )}
+                </div>
+
+                <div className={styles.field__container}>
+                  <TextField
+                    sx={
+                      errors.securityCode && touched.securityCode
+                        ? {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #e32224",
+                          }
+                        : {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #bdbdbd",
+                          }
+                    }
+                    label="Security Code (CVV)"
+                    type="password"
+                    name="securityCode"
+                    variant="filled"
+                    value={values.securityCode}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.securityCode && touched.securityCode ? (
+                    <p className={styles.validation__error}>
+                      {errors.securityCode}
+                    </p>
+                  ) : (
+                    <span className={styles.default}></span>
+                  )}
+                </div>
+              </div>
+
+              <div className={styles.name__container}>
+                <div className={styles.field__container}>
+                  <TextField
+                    sx={
+                      errors.zip && touched.zip
+                        ? {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #e32224",
+                          }
+                        : {
+                            ...textFieldCostumization,
+                            width: "242px",
+                            border: "1px solid #bdbdbd",
+                          }
+                    }
+                    label="Billing Zip Code"
+                    type="text"
+                    name="zip"
+                    variant="filled"
+                    value={values.zip}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.zip && touched.zip ? (
+                    <p className={styles.validation__error}>{errors.zip}</p>
+                  ) : (
+                    <span className={styles.default}></span>
+                  )}
+                </div>
+              </div>
+
+              <div className={styles.checkbox__container}>
+                <input
+                  className={styles.checkbox}
+                  type="checkbox"
+                  name="checkbox"
+                />
+                <p className={styles.checkbox__text}>
+                  By clicking Place Order, I accept the License Agreement,
+                  Auto-Renewal Policy and Privacy Notice.
+                </p>
+              </div>
+
+              <div className={styles.container__confirm}>
+                <p className={styles.text__confirm}>
+                  This payment will be processed in the United States
+                </p>
+                <button
+                  className={styles.button__confirm}
+                  type="submit"
+                  onClick={() => {
+                    touched.cardNumber = true;
+                  }}
+                >
+                  Place Order
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
+      ) : (
+        <div>some</div>
+      )}
     </div>
   );
 };
